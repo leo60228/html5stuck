3abf1ff99f6a1d1587737d42ba45608e07bf72e5
leo60228 <leo@60228.dev>
2020-05-19 15:03:36 -0400

    node.js is web scale

--- html5stuck_renderer/src/lib.rs
+++ html5stuck_renderer/src/lib.rs
@@ -1,6 +1,7 @@
 use anyhow::{anyhow, Context as _, Result};
 use askama::Template;
 use html5stuck_common::Page;
+use indicatif::{ProgressBar, ProgressStyle};
 use std::fs::{self, File};
 use std::io::BufReader;
 use std::path::Path;
@@ -37,9 +38,16 @@ pub fn batch_render(path: impl AsRef<Path>, output: impl AsRef<Path>) -> Result<
     let path = path.as_ref();
     let output = output.as_ref();
     fs::create_dir_all(output.join("story"))?;
-    for file in fs::read_dir(path).context("Getting directory listing")? {
+    let files: Vec<_> = fs::read_dir(path)
+        .context("Getting directory listing")?
+        .collect();
+    let len = files.len() as _;
+    let pb = ProgressBar::new(len);
+    pb.set_style(ProgressStyle::default_bar().template("{msg} {wide_bar} {pos}/{len}"));
+    pb.set_message("Generating story pages");
+    pb.set_draw_delta(len / 50);
+    for file in pb.wrap_iter(files.into_iter()) {
         let file = file.context("Iterating over directory listing")?;
-        println!("{:?}", file.file_name());
         let current = parse_page_file(&file.path()).context("Parsing current page")?;
         let next_num: usize = current
             .next
@@ -50,8 +58,12 @@ pub fn batch_render(path: impl AsRef<Path>, output: impl AsRef<Path>) -> Result<
         let next = parse_page_file(&path.join(format!("{}.json", next_num)))
             .context("Parsing next page")?;
         let rendered = render_page(&current, &next).context("Rendering page")?;
-        fs::write(&output.join(format!("story/{}.html", current.num)), &rendered)
-            .context("Writing rendered")?;
+        fs::write(
+            &output.join(format!("story/{}.html", current.num)),
+            &rendered,
+        )
+        .context("Writing rendered")?;
     }
+    pb.finish();
     Ok(())
 }
---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    map: {
+        inline: false,
+    },
+    plugins: [
+        require('postcss-advanced-variables')(),
+        require('postcss-calc')(),
+        require('postcss-preset-env')(),
+        require('postcss-nested')(),
+        process.env.NODE_ENV == 'production' ? require('cssnano')({
+            preset: 'default',
+        }) : require('stylefmt'),
+    ],
+}
