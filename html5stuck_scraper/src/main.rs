950df2ff41241b48a40c7e3b2a0d14b4abfdff0e
leo60228 <leo@60228.dev>
2020-05-19 17:02:25 -0400

    Add progress bar to scraper

--- html5stuck_scraper/src/main.rs
+++ html5stuck_scraper/src/main.rs
@@ -1,17 +1,23 @@
 use html5stuck_scraper::scrape_site;
+use indicatif::{ProgressBar, ProgressStyle};
 
 fn main() {
     let mut args = std::env::args().skip(1);
     let start = args.next().unwrap();
     let output = args.next().unwrap();
     std::fs::create_dir_all(&output).unwrap();
-    for x in scrape_site(Some(&output), &start) {
+    let len = 8054;
+    let pb = ProgressBar::new(len);
+    pb.set_style(ProgressStyle::default_bar().template("{msg} {wide_bar} {pos}/{len}"));
+    pb.set_message("Scraping site");
+    pb.set_draw_delta(len / 50);
+    for x in pb.wrap_iter(scrape_site(Some(&output), &start)) {
         let (html, x) = x.unwrap();
         let out_path = format!("{}/{}.json", output, x.num);
         let html_path = format!("{}/{}.html", output, x.num);
         let json = serde_json::to_string(&x).unwrap();
-        println!("{}: {}", out_path, json);
         std::fs::write(out_path, json).unwrap();
         std::fs::write(html_path, html).unwrap();
     }
+    pb.finish();
 }
