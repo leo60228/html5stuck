68a2c51a89642aa78112d8b4b5ee706bd23e67f3
leo60228 <leo@60228.dev>
2020-05-25 16:05:28 -0400

    Add without_holes function

--- html5stuck_common/src/lib.rs
+++ html5stuck_common/src/lib.rs
@@ -28,3 +28,33 @@ pub struct Page {
     pub next: Url,
     pub theme: Theme,
 }
+
+pub fn without_holes(page: usize) -> usize {
+    let holes = [2399, 3038, 3088, 6370, 7902, 7903, 7904];
+    let mut idx = holes
+        .iter()
+        .enumerate()
+        .find(|&(_, &x)| page < x)
+        .map(|x| x.0)
+        .unwrap_or(holes.len());
+    if page > 6245 && page < 7448 {
+        idx -= 1;
+    }
+    page - idx
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn without_holes() {
+        macro_rules! cases {
+            ($($a:expr, $b:expr),*) => {
+                $(assert_eq!(super::without_holes($a), $b, stringify!($a));)*
+            };
+        }
+        cases!(
+            1, 1, 2398, 2398, 2400, 2399, 3037, 3036, 3039, 3037, 3086, 3084, 3089, 3086, 6369,
+            6367, 6371, 6368, 7901, 7897, 7905, 7898, 8128, 8121
+        );
+    }
+}
